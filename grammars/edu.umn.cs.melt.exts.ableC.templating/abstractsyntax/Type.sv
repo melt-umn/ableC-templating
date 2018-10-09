5f348c4afb5b76a062ed39264d7406026cd57c01
Lucas Kramer <krame505@umn.edu>
2018-10-09 16:14:29 -0500

    Non-interfering overrides for some equations on templatedType to preseve pp for better errors, when possible

--- grammars/edu.umn.cs.melt.exts.ableC.templating/abstractsyntax/Type.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.templating/abstractsyntax/Type.sv
@@ -4,8 +4,20 @@ grammar edu:umn:cs:melt:exts:ableC:templating:abstractsyntax;
 abstract production templatedType
 top::Type ::= q::Qualifiers n::String args::[Type] resolved::Type
 {
+  -- Non-interfering overrides to preserve pp for better errors, as much as possible.
   top.lpp = pp"${terminate(space(), q.pps)}${text(n)}<${ppImplode(pp", ", map(\t::Type -> cat(t.lpp, t.rpp), args))}>";
   top.rpp = notext();
+  -- These are considered non-interfering since
+  -- typeName(top.baseTypeExpr, top.typeModifierExpr).* is equivalent to
+  -- typeName(top.forward.baseTypeExpr, top.forward.typeModifierExpr).*
+  top.baseTypeExpr =
+    templateTypedefTypeExpr(
+      q,
+      name(n, location=builtin),
+      foldr(
+        consTypeName, nilTypeName(),
+        map(\ t::Type -> typeName(t.baseTypeExpr, t.typeModifierExpr), args)));
+  top.typeModifierExpr = baseTypeExpr();
   
   forwards to resolved;
 }
@@ -17,7 +29,9 @@ Type ::= q::Qualifiers n::String args::[Type] env::Decorated Env
     templateTypedefTypeExpr(
       q,
       name(n, location=builtin),
-      foldr(consTypeName, nilTypeName(), map(\ t::Type -> typeName(directTypeExpr(t), baseTypeExpr()), args)));
+      foldr(
+        consTypeName, nilTypeName(),
+        map(\ t::Type -> typeName(t.baseTypeExpr, t.typeModifierExpr), args)));
   result.env = env;
   result.returnType = nothing();
   result.givenRefId = nothing();
