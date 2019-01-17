268825e837bf8dcc9f17769d3da3cd6ffcf8ebf4
Lucas Kramer <krame505@umn.edu>
2019-01-17 15:09:38 -0600

    Simplify logic instantiation productions by parameterizing with [Type] instead of TypeNames

--- grammars/edu.umn.cs.melt.exts.ableC.templating/abstractsyntax/Instantiation.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.templating/abstractsyntax/Instantiation.sv
@@ -10,7 +10,10 @@ top::Expr ::= n::Name ts::TypeNames
   
   forwards to
     injectGlobalDeclsExpr(
-      consDecl(templateExprInstDecl(n, decTypeNames(ts)), nilDecl()),
+      foldDecl(
+        (if !null(ts.errors) then [warnDecl(ts.errors)] else []) ++
+        ts.decls ++
+        [templateExprInstDecl(n, ts.typereps)]),
       directRefExpr(name(templateMangledName(n.name, ts.typereps), location=top.location), location=top.location),
       location=top.location);
 }
@@ -25,7 +28,10 @@ top::Expr ::= n::Name ts::TypeNames a::Exprs
   
   forwards to
     injectGlobalDeclsExpr(
-      consDecl(templateExprInstDecl(n, decTypeNames(ts)), nilDecl()),
+      foldDecl(
+        (if !null(ts.errors) then [warnDecl(ts.errors)] else []) ++
+        ts.decls ++
+        [templateExprInstDecl(n, ts.typereps)]),
       directCallExpr(name(templateMangledName(n.name, ts.typereps), location=top.location), a, location=top.location),
       location=top.location);
 }
@@ -43,157 +49,129 @@ top::BaseTypeExpr ::= q::Qualifiers n::Name ts::TypeNames
   
   forwards to
     injectGlobalDeclsTypeExpr(
-      consDecl(templateTypeExprInstDecl(q, n, decTypeNames(ts)), nilDecl()),
+      foldDecl(
+        (if !null(ts.errors) then [warnDecl(ts.errors)] else []) ++
+        ts.decls ++
+        [templateTypeExprInstDecl(q, n, ts.typereps)]),
       typedefTypeExpr(q, name(templateMangledName(n.name, ts.typereps), location=builtin)));
 }
 
 abstract production templateExprInstDecl
-top::Decl ::= n::Name ts::TypeNames
+top::Decl ::= n::Name ts::[Type]
 {
-  top.pp = pp"inst ${n.pp}<${ppImplode(pp", ", ts.pps)}>;";
-  top.substituted = templateExprInstDecl(n, ts.substituted); -- Don't substitute n
+  top.pp = pp"inst ${n.pp}<${ppImplode(pp", ", zipWith(cat, map((.lpp), ts), map((.rpp), ts)))}>;";
+  top.substituted = top; -- Don't substitute n
   
   local templateItem::Decorated TemplateItem = n.templateItem;
   
   local localErrors::[Message] =
-    ts.errors ++
     if !null(n.templateLookupCheck)
     then n.templateLookupCheck
     else if !templateItem.isItemValue
     then [err(n.location, s"${n.name} is not a value")]
-    else if !templateItem.isItemError && ts.count != length(templateItem.templateParams)
+    else if !templateItem.isItemError && length(ts) != length(templateItem.templateParams)
     then [err(
             n.location,
             s"Wrong number of template parameters for ${n.name}, " ++
-            s"expected ${toString(length(templateItem.templateParams))} but got ${toString(ts.count)}")]
+            s"expected ${toString(length(templateItem.templateParams))} but got ${toString(length(ts))}")]
     else if !null(fwrd.errors)
     then
       [nested(
          n.location,
-         s"In instantiation ${n.name}<${show(80, ppImplode(pp", ", ts.pps))}>",
+         s"In instantiation ${n.name}<${implode(", ", map(showType, ts))}>",
          fwrd.errors)]
     else [];
   
-  local mangledName::String = templateMangledName(n.name, ts.typereps);
+  local mangledName::String = templateMangledName(n.name, ts);
   
   local fwrd::Decl =
-    decls(
-      foldDecl(
-        ts.decls ++
-        if !null(lookupValue(mangledName, addEnv(ts.defs, ts.env)))
-        then []
-        else
-          [substDecl(
-             zipWith(
-               typedefSubstitution,
-               templateItem.templateParams,
-               map(directTypeExpr, ts.typereps)),
-             templateItem.decl(name(mangledName, location=builtin)))]));
+    if !null(lookupValue(mangledName, top.env))
+    then decls(nilDecl())
+    else
+      substDecl(
+        zipWith(
+          typedefSubstitution,
+          templateItem.templateParams,
+          map(directTypeExpr, ts)),
+        templateItem.decl(name(mangledName, location=builtin)));
   fwrd.isTopLevel = true;
   fwrd.env = top.env;
   fwrd.returnType = nothing();
   
   forwards to
-    if templateItem.isItemError || containsErrorType(ts.typereps) || !null(localErrors)
+    if templateItem.isItemError || containsErrorType(ts) || !null(localErrors)
     then
-      decls(
-        foldDecl(
-          ts.decls ++
-          [variableDecls(
-             nilStorageClass(), nilAttribute(),
-             errorTypeExpr(localErrors),
-             consDeclarator(
-               declarator(
-                 name(mangledName, location=builtin),
-                 baseTypeExpr(),
-                 nilAttribute(),
-                 nothingInitializer()),
-               nilDeclarator()))]))
+      variableDecls(
+        nilStorageClass(), nilAttribute(),
+        errorTypeExpr(localErrors),
+        consDeclarator(
+          declarator(
+            name(mangledName, location=builtin),
+            baseTypeExpr(),
+            nilAttribute(),
+            nothingInitializer()),
+          nilDeclarator()))
     else decDecl(fwrd);
 }
 
 abstract production templateTypeExprInstDecl
-top::Decl ::= q::Qualifiers n::Name ts::TypeNames
+top::Decl ::= q::Qualifiers n::Name ts::[Type]
 {
-  top.pp = pp"inst ${terminate(space(), q.pps)}${n.pp}<${ppImplode(pp", ", ts.pps)}>;";
-  top.substituted = templateTypeExprInstDecl(q, n, ts.substituted); -- Don't substitute n
+  top.pp = pp"inst ${terminate(space(), q.pps)}${n.pp}<${ppImplode(pp", ", zipWith(cat, map((.lpp), ts), map((.rpp), ts)))}>;";
+  top.substituted = top; -- Don't substitute n
   
   local templateItem::Decorated TemplateItem = n.templateItem;
   
   local localErrors::[Message] =
-    ts.errors ++
     if !null(n.templateLookupCheck)
     then n.templateLookupCheck
     else if !templateItem.isItemType
     then [err(n.location, s"${n.name} is not a type")]
-    else if !templateItem.isItemError && ts.count != length(templateItem.templateParams)
+    else if !templateItem.isItemError && length(ts) != length(templateItem.templateParams)
     then [err(
             n.location,
             s"Wrong number of template parameters for ${n.name}, " ++
-            s"expected ${toString(length(templateItem.templateParams))} but got ${toString(ts.count)}")]
+            s"expected ${toString(length(templateItem.templateParams))} but got ${toString(length(ts))}")]
     else if !null(fwrd.errors)
     then
       [nested(
          n.location,
-         s"In instantiation ${n.name}<${show(80, ppImplode(pp", ", ts.pps))}>",
+         s"In instantiation ${n.name}<${implode(", ", map(showType, ts))}>",
          fwrd.errors)]
     else [];
   
-  local mangledName::String = templateMangledName(n.name, ts.typereps);
-  local mangledRefId::String = templateMangledRefId(n.name, ts.typereps);
+  local mangledName::String = templateMangledName(n.name, ts);
+  local mangledRefId::String = templateMangledRefId(n.name, ts);
   
   local fwrd::Decl =
-    decls(
-      foldDecl(
-        ts.decls ++
-        if !null(lookupValue(mangledName, addEnv(ts.defs, ts.env)))
-        then []
-        else
-          [substDecl(
-             refIdSubstitution(s"edu:umn:cs:melt:exts:ableC:templating:${n.name}", mangledRefId) ::
-             zipWith(
-               typedefSubstitution,
-               templateItem.templateParams,
-               map(directTypeExpr, ts.typereps)),
-             templateItem.decl(name(mangledName, location=builtin)))]));
+    if !null(lookupValue(mangledName, top.env))
+    then decls(nilDecl())
+    else
+      substDecl(
+        refIdSubstitution(s"edu:umn:cs:melt:exts:ableC:templating:${n.name}", mangledRefId) ::
+        zipWith(
+          typedefSubstitution,
+          templateItem.templateParams,
+          map(directTypeExpr, ts)),
+        templateItem.decl(name(mangledName, location=builtin)));
   fwrd.isTopLevel = true;
   fwrd.env = top.env;
   fwrd.returnType = nothing();
   
   forwards to
-    if templateItem.isItemError || containsErrorType(ts.typereps) || !null(localErrors)
+    if templateItem.isItemError || containsErrorType(ts) || !null(localErrors)
     then
-      decls(
-        foldDecl(
-          ts.decls ++
-          [typedefDecls(
-             nilAttribute(),
-             errorTypeExpr(localErrors),
-             consDeclarator(
-               declarator(
-                 name(mangledName, location=builtin),
-                 baseTypeExpr(),
-                 nilAttribute(),
-                 nothingInitializer()),
-               nilDeclarator()))]))
+      typedefDecls(
+        nilAttribute(),
+        errorTypeExpr(localErrors),
+        consDeclarator(
+          declarator(
+            name(mangledName, location=builtin),
+            baseTypeExpr(),
+            nilAttribute(),
+            nothingInitializer()),
+          nilDeclarator()))
     else decDecl(fwrd);
-}
-
-abstract production decTypeNames
-top::TypeNames ::= ts::Decorated TypeNames
-{
-  top.host = ts.host;
-  top.lifted = ts.lifted;
-  top.pps = ts.pps;
-  top.typereps = ts.typereps;
-  top.count = ts.count;
-  top.errors := ts.errors;
-  top.globalDecls := ts.globalDecls;
-  top.decls = ts.decls;
-  top.defs := ts.defs;
-  top.freeVariables := ts.freeVariables;
-  
-  forwards to new(ts);
 }
 
 function templateMangledName
