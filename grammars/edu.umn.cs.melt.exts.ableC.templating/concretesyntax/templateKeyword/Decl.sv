3130dfcb2a87cf2a3ce33dd2e36cbc05a1c32889
Lucas Kramer <krame505@umn.edu>
2019-12-09 16:03:13 -0600

    Rename lexer classes

--- grammars/edu.umn.cs.melt.exts.ableC.templating/concretesyntax/templateKeyword/Decl.sv
+++ grammars/edu.umn.cs.melt.exts.ableC.templating/concretesyntax/templateKeyword/Decl.sv
@@ -2,4 +2,4 @@ grammar edu:umn:cs:melt:exts:ableC:templating:concretesyntax:templateKeyword;
 
 imports edu:umn:cs:melt:ableC:concretesyntax;
 
-marking terminal Template_t 'template' lexer classes {Keyword, ScopedReserved};
+marking terminal Template_t 'template' lexer classes {Keyword, Global};
